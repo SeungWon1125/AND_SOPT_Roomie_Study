608460423e2e1acf8831733f0c64cbaa897d05d4
won <andrew112517@naver.com>
2025-01-02 15:52:34 +0900

    [feat/#3] TourViewModel 추가

--- Roomie_Study/Roomie_Study/Week1/TourViewModel.swift
+++ Roomie_Study/Roomie_Study/Week1/TourViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+final class TourViewModel {
+    
+    // MARK: - Property
+    
+    var tourState = Observable<TourState>.init(.applyTour)
+    
+    // MARK: - Functions
+    
+    func applyTour() {
+        tourState.value = .startTour
+    }
+    
+    func startTour() {
+        tourState.value = .writeContract
+    }
+    
+    func writeContract() {
+        tourState.value = .endTour
+    }
+    
+}
