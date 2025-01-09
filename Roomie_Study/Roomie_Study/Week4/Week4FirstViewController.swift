1b7e51f4a1846a9e2f46d714069608553ebf8e51
won <andrew112517@naver.com>
2025-01-09 15:43:42 +0900

    [feat/#5] 다음주차 뷰..

---
+++ Roomie_Study/Roomie_Study/Week4/Week4FirstViewController.swift
@@ -0,0 +1,28 @@
+//
+//  Week4FirstViewController.swift
+//  Roomie_Study
+//
+//  Created by 김승원 on 1/9/25.
+//
+
+import UIKit
+
+class Week4FirstViewController: UIViewController {
+    
+    // MARK: - Property
+    
+    private let rootView = Week4FirstView()
+    
+    // MARK: - LifeCycle
+    
+    override func loadView() {
+        view = rootView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
