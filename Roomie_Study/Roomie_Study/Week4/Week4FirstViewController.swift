c1798c77c632f4e6383ba44b804d16d1f7cb10ac
won <andrew112517@naver.com>
2025-01-09 17:20:47 +0900

    [feat/#7] 구현 과제 1

--- Roomie_Study/Roomie_Study/Week4/Week4FirstViewController.swift
+++ Roomie_Study/Roomie_Study/Week4/Week4FirstViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import Combine
+
+import CombineCocoa
 
 class Week4FirstViewController: UIViewController {
     
@@ -13,16 +16,55 @@ class Week4FirstViewController: UIViewController {
     
     private let rootView = Week4FirstView()
     
+    private let tapSubject = CurrentValueSubject<Int, Never>(0)
+    private let cancelBag = CancelBag()
+    
     // MARK: - LifeCycle
     
     override func loadView() {
         view = rootView
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        setAction()
     }
-
+    
+    // MARK: - Functions
+    
+    private func setAction() {
+        rootView.upButton
+            .tapPublisher
+            .sink { [weak self] _ in
+                guard let self else { return }
+                self.tapSubject.send(tapSubject.value + 1)
+            }
+            .store(in: cancelBag)
+        
+        rootView.downButton
+            .tapPublisher
+            .sink { [weak self] _ in
+                guard let self else { return }
+                self.tapSubject.send(tapSubject.value - 1 >= 0 ? tapSubject.value - 1 : 0)
+            }
+            .store(in: cancelBag)
+        
+        tapSubject
+            .map { "\($0*2)"}
+            .sink { [weak self] text in
+                guard let self else { return }
+                self.rootView.doubleNumberLabel.text = text
+            }
+            .store(in: cancelBag)
+        
+        tapSubject
+            .map { "\($0)"}
+            .sink { [weak self] text in
+                guard let self else { return }
+                self.rootView.numberLabel.text = text
+            }
+            .store(in: cancelBag)
+    }
+    
 }
